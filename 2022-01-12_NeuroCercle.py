73e031e5ec3859a1248d0ddabb4247db7cebadc2
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2022-01-11 09:31:59 +0100

    updating slides

--- 2022-01-12_NeuroCercle.py
+++ 2022-01-12_NeuroCercle.py
@@ -56,8 +56,8 @@ meta = dict(
  short_title='Des illusions aux hallucinations visuelles',
  title='Des illusions aux hallucinations visuelles: <BR> une porte sur la perception',
  conference_url='https://www.jnlf.fr/agenda/jnlf-lille-2019',
- short_conference='Vedène 2020',
- conference='Cinéma et sciences : rencontre avec les élèves du lycée des métiers',
+ short_conference='Neurocercle',
+ conference='Neurocercle : Découvrir les neurosciences à Grenoble',
  location='Vedène (France)',
  abstract="""Les illusions visuelles sont des créations d'artistes, de scientifiques et plus récemment, grâce aux réseaux sociaux, du grand public qui proposent des situations souvent incongrues, dans lesquelles l'eau remonte une cascade, les personnes volent dans les airs ou des serpents se mettent à tourner. Au-delà de leur indéniable coté ludique, ces illusions nous apprennent beaucoup sur le fonctionnement du cerveau, notamment quand celles-ci se transforment en hallucinations visuelles, dépassant ainsi les limites des capacités de notre perception. En tant que chercheur en Neurosciences à l'Institut de Neurosciences de la Timone à Marseille, je vous dévoilerai des aspects du fonctionnement du cerveau qui sont souvent méconnus. En particulier, nous verrons pourquoi un magicien peut tromper nos sens ou comment des objets peuvent voyager dans le temps. Surtout nous essaierons de comprendre le fonctionnement de notre perception visuelle sur les bases d'une théorie de la vision non pas comme une simple caméra qui enregistre des images mais comme un processus actif en relation avec le monde qui nous entoure.""",
  summary="""Les objectifs sont :
@@ -71,7 +71,7 @@ meta = dict(
  time_start='10:00:00',
  time_end='16:00:00',
  url=f'https://laurentperrinet.github.io/talk/{slugify(tag)}',
- sections=['Illusions visuelles & hallucinations',
+ sections=['Universalité des illusions visuelles',
            'Une neuro-anatomie fonctionnelle des illusions & hallucinations?',
            'Coute que coute, prédire le présent',
            ]
